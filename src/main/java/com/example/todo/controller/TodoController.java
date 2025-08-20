91cbb9cfca04dc0a023a9caf071ca47a88fa9a1b
오혜성 <ohs020105@gmail.com>
2025-08-21 01:16:03 +0900

    changes: 커밋 테스트 확인용

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -3,6 +3,7 @@ package com.example.todo.controller;
 import com.example.todo.domain.Todo;
 import com.example.todo.service.TodoService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 // @RestController 선언: 해당 클래스가 REST API 요청을 처리하는 컨트롤러임을 선언 
@@ -10,12 +11,12 @@ import java.util.List;
 // 기본 URL 매핑: 모든 엔드포인트에서 "/todos"를 기본 경로로 사용
 @RequestMapping("/todos")
 public class TodoController {
-    
+
     // TodoService를 final 필드로 선언하여 불변셩을 보장 (생성자 주입을 통해 초기화 됨)
     private final TodoService todoService;
-    
+
     // 생성자 주입: TodoService 의존성을 생성자를 통해 주입받음
-    public TodoController(TodoService todoService){
+    public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
 
@@ -41,13 +42,14 @@ public class TodoController {
     @PutMapping("/{id}/status")
     public Todo toggleTodoStatus(
             // URL 경로의 {id}값을 파라미터로 받아옴
+
             @PathVariable("id") Long id,
             // 요청 파라미터로 전달된 done 상태 값을 받아옴
             @RequestParam("status") boolean done) {
         // TodoService의 toggleTodoStatus() 메서드를 호출하여 완료 상태를 업데이트한 Todo 반환
         return todoService.toggleTodoStatus(id, done);
     }
-    
+
     // GET(가져오기) 요청 : 특정 id의 Todo 엔티티를 조회하는 엔드포인트
     @GetMapping("/{id}")
     public Todo findTodoById(
@@ -56,6 +58,7 @@ public class TodoController {
         // todoService의 findTodoById() 메서드를 호출하여 해당 Todo를 조회 후 반환
         return todoService.findTodoById(id);
     }
+
     // GET 요청 : 모든 Todo 리스트를 조회하는 엔드포인트
     @GetMapping
     public List<Todo> findAllTodos() {
